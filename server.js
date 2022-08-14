b3ff442a51344c6968275e60a4955e6c987c61e2
bhawani003 <bhawani.singh-chauhan@cobovec.com>
2022-08-14 18:23:08 +0530

    Initial routes changes

--- server.js
+++ server.js
@@ -6,17 +6,21 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts")
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const indexRouter = require("./routes")
+const indexRouter = require("./routes/index")
+const authorsRouter = require('./routes/authors')
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 mongoose.connect(process.env.DATABASE_URL,() => console.log('connedted'), e => console.error(e));
 
 app.use("/", indexRouter)
+app.use("/authors", authorsRouter)
 
 app.listen(process.env.PORT || 3000)
